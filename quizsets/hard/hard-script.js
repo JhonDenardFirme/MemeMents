53418c7e1793ff97460c0a47fec48c44c49f8b2c
Eronixy <erroniz21@gmail.com>
2025-04-27 21:54:45 +0800

    Imported all GIFS to website

--- quizsets/hard/hard-script.js
+++ quizsets/hard/hard-script.js
@@ -502,7 +502,7 @@ function updateContent(index) {
     elements.options.textB.textContent = quizData[index].options.b;
     elements.options.textC.textContent = quizData[index].options.c;
     elements.options.textD.textContent = quizData[index].options.d;
-    elements.quiz.memeImage.src = `../../images/quiz-assets/meme-img-easy/${(index+1).toString()}.jpg`;
+    elements.quiz.memeImage.src = `../../images/quiz-assets/H${(index + 1).toString()}.gif`;
 
     setTimeout(() => {
         audioSystem.playQuestionVoice(index);
